29cc45558edc645be6dce253e07722f0d47531be
Henrik Fricke <henrik.fricke@jimdo.com>
2017-12-06 11:51:00 +0100

    Clean up main.go file

--- main.go
+++ main.go
@@ -1,33 +1,39 @@
 package main
 
 import (
+	"github.com/HenrikFricke/go-postgres-example/api"
+	"github.com/HenrikFricke/go-postgres-example/repository"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// func InitDb() *gorm.DB {
-// 	// Openning file
-// 	db, err := gorm.Open("sqlite3", "./data.db")
-// 	db.LogMode(true)
-// 	// Error
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	// Creating the table
-// 	if !db.HasTable(&models.Users{}) {
-// 		db.CreateTable(&models.Users{})
-// 		db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&models.Users{})
-// 	}
+func initDb() *gorm.DB {
+	// Openning file
+	db, err := gorm.Open("sqlite3", "./data.db")
+	db.LogMode(true)
+	// Error
+	if err != nil {
+		panic(err)
+	}
+	// Creating the table
+	if !db.HasTable(&repository.Users{}) {
+		db.CreateTable(&repository.Users{})
+		db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&repository.Users{})
+	}
 
-// 	return db
-// }
+	return db
+}
 
 func main() {
-	// r := gin.Default()
-	// v1 := r.Group("api/v1")
+	db := initDb()
+	repository := repository.Repository{db}
+	api := api.API{&repository}
 
-	// v1.POST("/users", PostUser)
-	// v1.GET("/users", GetUsers)
-	// v1.GET("/users/:id", GetUser)
+	r := gin.Default()
+	v1 := r.Group("api/v1")
 
-	// r.Run(":8080")
+	v1.GET("/users/:id", api.GetUser)
+
+	r.Run(":8080")
 }
